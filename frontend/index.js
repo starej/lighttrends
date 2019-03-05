820f6a62d06c406ef266bb344ca0de8fa3aa4923
starej <48224786+starej@users.noreply.github.com>
2019-03-05 15:09:19 +0100

    Added license info header

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,4 @@
+/*
 ############################################################
 # 
 # Radiance Light Trends is a software for selecting regions of Earth and examining the trend in light emissions observed by satellite.
@@ -15,6 +16,7 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the Licence for the specific language governing permissions and limitations under the Licence.
 # 
 ############################################################
+*/
 import jQuery from "jquery";
 $ = window.$ = window.jQuery = jQuery;
 import 'jquery-ui-bundle';
